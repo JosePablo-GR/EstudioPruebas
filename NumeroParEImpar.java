ebe195ee1cfc773382c463c0c760df9dc5429cac
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-10-03 16:12:42 -0500

    Update NumeroParEImpar.java

--- NumeroParEImpar.java
+++ NumeroParEImpar.java
@@ -1,20 +1,8 @@
-// Importa las clases necesarias.
 import java.util.Scanner;
 
-/**
- * Clase NumeroParEImpar.java
- * Programa que nos dice si un número es par.
- * 
- * @author Rodrigo Alejandro Sánchez Morales.
- * @version Marzo 2020.
- */
 public class NumeroParEImpar {
 
-    /**
-     * Método main (el cual es el punto inicial de la JVM para comenzar con la
-     * ejecución de un programa de Java).
-     */
-    public static void main(String[] args) {
+       public static void main(String[] args) {
 
         // Declaración de variables.
         long numero;
@@ -39,17 +27,9 @@ public class NumeroParEImpar {
             System.out.println("es impar.");
         }
 
-        // Esta instrucción que contiene solamente una instrucción se puede 
-        // escribir sin llaves pero es recomendable ponerlas siempre.
-        // if(numero % 2 == 0)
-        //     System.out.println("es par.");
-        // else
-        //     System.out.println("es impar.");
-        
-
         // Cerramos el Scanner.
         lector.close();
 
     } // Fin main.
 
-} // Fin NumeroParEImpar.
+} // Fin clase
