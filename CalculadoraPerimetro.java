aefbe5075c2cb865a5ffed56402a11d083c87651
José Pablo GR <89881634+JosePablo-GR@users.noreply.github.com>
2022-10-03 16:11:33 -0500

    Toques

--- CalculadoraPerimetro.java
+++ CalculadoraPerimetro.java
@@ -1,21 +1,7 @@
-// Importa las clases necesarias.
 import java.util.Scanner;
-import static java.lang.System.*;   // Evita escribir la palabra System.
 
-/**
- * Clase CalculadoraPerimetro.java
- * Programa que calcula el perímetro de una figura.
- * 
- * @author Rodrigo Alejandro Sánchez Morales.
- * @version Febrero 2020.
- * 
- */
 public class CalculadoraPerimetro {
-
-    /**
-     * Método main (el cual es el punto inicial de la JVM para comenzar con la
-     * ejecución de un programa de Java).
-     */
+    
     public static void main(String[] args) {
 
         // Declaración de variables.
@@ -25,28 +11,28 @@ public class CalculadoraPerimetro {
         double longitud, perimetro;
 
         // Creamos el Scanner.
-        lector = new Scanner(in);
+        lector = new Scanner(System.in);
 
         // Mensaje inicial del programa.
-        out.println("Calculo el perimetro de figuras geométricas regulares.");
+        out.println("Este programa calcula el perimetro de figuras geométricas regulares.");
 
         // Pedimos el nombre del usuario.
         out.println("Ingresa tu nombre:");
         nombre = lector.nextLine();
 
         // Ahora pedimos el número de lados.
-        out.println("Ingresa el número de lados:");
+        System.out.println("Ingresa el número de lados:");
         lados = lector.nextShort();
 
         // Ahora pedimos la longitud de cada lado.
-        out.println("Ingresa la longitud de cada lado:");
+        System.out.println("Ingresa la longitud de cada lado:");
         longitud = lector.nextDouble();
 
         // Calcula el perímetro de la figura geométrica regular.
         perimetro = lados * longitud;
 
         // Ahora juntamos toda la información del usuario.
-        out.println("¡Hola, " + nombre + "! :)\n" +
+        System.out.println("Hola, " + nombre + " :)\n" +
             "Ingresaste " + lados + " lados de " + longitud + " de longitud.\n" +
             "El perímetro de tu figura es " + perimetro + "."
         );
@@ -56,4 +42,4 @@ public class CalculadoraPerimetro {
 
     } // Fin main.
 
-} // Fin CalculadoraPerimetro.
+} // Fin clase
